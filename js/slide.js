d7c8a626e550cc416ff3f11b941e89fdfbe98b72
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-09-29 16:14:59 -0300

    Update e correção de bugs

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,10 @@
 new Swiper(".mySwiper", {
   slidesPerView: "auto",
   spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   freeMode: true,
   loop: true,
 });
