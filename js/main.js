20cadd6679ed964c0ef45d379a3905d52efa6175
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-11-06 12:29:36 -0300

    Update projetcs and remove comments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const arrowTop = document.querySelector(".arrow-top");
 const arrowBottom = document.querySelector(".arrow-bottom");
 
-// ARROW-TO-SECTION
 const arrowToSection = () => {
-  window.scrollTo(0, 800);
+  window.scrollTo(0, 790);
 };
 
-// ARROW-TO-TOP
 const showArrowToTop = () => {
   if (scrollY > 650) {
     arrowTop.classList.add("active");
@@ -19,7 +17,6 @@ const arrowToTop = () => {
   window.scrollTo(0, 0);
 };
 
-// MODAL
 const body = document.querySelector("body");
 const wrapperModal = document.querySelector(".wrapper-modal");
 const modal = document.querySelector(".modal");
@@ -28,21 +25,17 @@ const btnNo = document.querySelector(".btn-n");
 
 const openModal = () => {
   body.style.overflow = "hidden";
-  modal.style.visibility = "visible";
   wrapperModal.classList.add("active");
 };
 
 const closeModal = () => {
   body.style.overflow = "visible";
-  modal.style.visibility = "hidden";
   wrapperModal.classList.remove("active");
 };
 
-// BTN-EVENTS
 btnContact.addEventListener("click", openModal);
 btnNo.addEventListener("click", closeModal);
 
-// ARROW-EVENTS
 window.addEventListener("scroll", showArrowToTop);
 arrowTop.addEventListener("click", arrowToTop);
 arrowBottom.addEventListener("click", arrowToSection);
